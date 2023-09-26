80c0d7616f000cceba04906c52a4cdcc72b2bdaa
kang <287603490@qq.com>
2023-09-26 17:46:56 +0800

    添加模型等文件，可直接运行

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ You can download the face-api.min.js library from [face-api official repository]
 ### models
 
 You can download the models I use in the video [here](https://github.com/justadudewhohacks/face-api.js/tree/master/weights).
+
+## 调试注意
+
+- 在 VSCode 中安装 live server 插件，即可直接运行html文件
+- 在项目根目录下，创建labels文件夹，在下面添加要识别的人物的头像图片，子文件夹的名称作为label
+- --tolerance 来实现这个功能，默认的容错率是0.6，容错率越低，识别越严格准确
--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
 ]).then(startWebcam);
+
+// Promise.all([
+//   faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+//   faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+// ])
 
 function startWebcam() {
   navigator.mediaDevices
@@ -21,17 +27,25 @@ function startWebcam() {
 }
 
 function getLabeledFaceDescriptions() {
-  const labels = ["Felipe", "Messi", "Data"];
+  // labels下面的子文件夹名称，作为label
+  const labels = [ "Mine", "Obama"];
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+        const imgFile = `./labels/${label}/${i}.jpg`;
+        console.log(imgFile);
+        const img = await faceapi.fetchImage(imgFile);
         const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
           .withFaceDescriptor();
-        descriptions.push(detections.descriptor);
+        if (!detections) {
+          console.log(`no faces detected for ${label}`);
+        } else {
+          console.log(`face detected for ${label}`);
+          descriptions.push(detections.descriptor);
+        }
       }
       return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
@@ -40,7 +54,7 @@ function getLabeledFaceDescriptions() {
 
 video.addEventListener("play", async () => {
   const labeledFaceDescriptors = await getLabeledFaceDescriptions();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6); // 0.6 is the distance threshold, 越小越严格
 
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
